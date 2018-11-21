8ffa965bd1ed5b4083854855a673b9c91d835780
AvutE <tuva@etavo.com>
2018-11-21 13:54:34 +0100

    Connection mLab up

---
+++ models/Respository.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var db_url = 'mongodb://winnie:wine234@ds113454.mlab.com:13454/wineforwinegoofs';
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db_url, {userNewUrlPaser: true})
+    .then(function(){
+            console.log('Connected to mLab MongoDB');
+    })
+    .catch(function(error){
+        console.log('Error connecting to mLab MongoDB');
+        console.log(error);
+    });
